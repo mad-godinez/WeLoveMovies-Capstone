ed63e703a3615651afa2e2340c84ceb5c4e21c2a
Mad Godinez <madgodinez@gmail.com>
2022-04-10 17:04:19 -0500

    fixed procfile

--- backend-WLM/src/server.js
+++ backend-WLM/src/server.js
@@ -1,14 +1,26 @@
-const { PORT = 5000 } = process.env;
-
-const app = require("./app");
 const knex = require("./db/connection");
+const express = require('express')
+const cors = require('cors')
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+const app = express()
+const router = express.Router()
+const PORT = process.env.PORT || 5000
 
-// knex.migrate
-//   .latest()
+router.get('/', cors(), (req, res) => {
+  res.json({ message: 'Hello Heroku!' });
+})
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `);
+})
+
+knex.migrate
+  .latest()
 //   .then((migrations) => {
 //     console.log("migrations", migrations);
 //     app.listen(PORT, listener);
 //   })
 //   .catch(console.error);
+module.exports = app
