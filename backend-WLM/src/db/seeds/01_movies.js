6f88771bc05c2622babed44b4c478e236c9b584f
Mad Godinez <madgodinez@gmail.com>
2022-04-11 15:28:04 -0500

    fixed a broken link for pan's labrynth img

--- backend-WLM/src/db/seeds/01_movies.js
+++ backend-WLM/src/db/seeds/01_movies.js
@@ -90,7 +90,7 @@ exports.seed = async function (knex) {
       description:
         "In 1944 Falangist Spain, a girl, fascinated with fairy-tales, is sent along with her pregnant mother to live with her new stepfather, a ruthless captain of the Spanish army. During the night, she meets a fairy who takes her to an old faun in the center of the labyrinth. He tells her she's a princess, but must prove her royalty by surviving three gruesome tasks. If she fails, she will never prove herself to be the true princess and will never see her real father, the king, again...",
       image_url:
-        "https://m.media-amazon.com/images/M/MV5BMTU3ODg2NjQ5NF5BMl5BanBnXkFtZTcwMDEwODgzMQ@@._V1_Ratio0.6791_AL_.jpg",
+        "https://aiptcomics.com/wp-content/uploads/2018/09/1216x1824x2.jpg",
     },
     {
       title: "Dial M for Murder",
